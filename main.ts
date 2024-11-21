5f6733e73f483c7fa4e7b26f0c8f0ff5b4be23ac
Benjamin897-glitch <franckenbenjamin@gmail.com>
2024-11-21 11:26:09 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let getal = 0
+input.onButtonPressed(Button.A, function () {
+    if (getal) {
+        getal = 0
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (getal) {
+    	
+    } else {
+    	
+    }
+})
